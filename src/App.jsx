51a5acc52d332f5aa465cc123c2008ba50c7bfeb
m-brandao <miguel.brandao@feedzai.com>
2024-06-05 15:20:04 +0100

    adding html5 semantic structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Little Lemon Restaurant</h1>
+      <nav>Navbar</nav>
+      <header>Hero</header>
+      <main>
+        <section>Highlights</section>
+        <section>Testimonials</section>
+        <section>About</section>
+      </main>
+      <footer>Footer</footer>
     </div>
   );
 }
